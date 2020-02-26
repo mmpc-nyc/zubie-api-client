f75e4f90a616538379e284c095e6dea8854faef8
Marek Schir <schir2@gmail.com>
2020-02-26 14:51:33 -0500

    Building Package.

--- setup.py
+++ setup.py
@@ -5,12 +5,17 @@ with open("README.md", "r") as file_handler:
 
 setuptools.setup(
     name = 'schir2',
-    version = '1.0',
+    version = '1.1',
     author = 'Marek Schir',
     author_email = 'schir2@gmail.com',
     description = 'Zubie API Client',
     long_description = long_description,
     long_description_content_type = 'text/markdown',
-
-
+    url = 'https://github.com/schir2/zubie-api-client',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6'
 )
